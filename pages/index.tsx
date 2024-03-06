79f6d304f810804170d4eee7002ef51f5495a35b
Bianca Chami <b.chami39@gmail.com>
2024-03-06 13:04:43 +0000

    feat: updated index file

--- pages/index.tsx
+++ pages/index.tsx
@@ -180,10 +180,30 @@ export const getStaticProps: GetStaticProps = async ({ locale }) => {
     currentLocale.directus
   );
 
-  services?.sort((a, b) =>
-    a.name?.normalize().localeCompare(b.name?.normalize())
+  const providersArray = await getDirectusProviders(
+    directus,
+    DIRECTUS_COUNTRY_ID
   );
 
+  const uniqueProvidersIdsSet = new Set(services.flatMap((x) => x.provider.id));
+  const uniqueProvidersIdsArray = Array.from(uniqueProvidersIdsSet);
+
+  const providers = providersArray
+    .filter((x) => uniqueProvidersIdsArray.includes(x.id))
+    .sort((a, b) => a.name?.normalize().localeCompare(b.name?.normalize()));
+
+  const enhancedServices = services.map((service) => {
+    const providerDetails = providers.find(
+      (provider) => provider.id === service.provider.id
+    );
+    return providerDetails
+      ? { ...service, provider: providerDetails }
+      : service;
+  });
+
+  enhancedServices?.sort((a, b) =>
+    a.name?.normalize().localeCompare(b.name?.normalize())
+  );
   const uniqueAccessibilityIdsSet = new Set(
     services.flatMap((x) =>
       x.Accessibility.map(
@@ -203,9 +223,6 @@ export const getStaticProps: GetStaticProps = async ({ locale }) => {
   const uniqueRegionsIds = new Set(services.map((service) => service.region));
 
   const uniqueCitiesIds = new Set(services.map((service) => service.city));
-
-  const uniqueProvidersIdsSet = new Set(services.flatMap((x) => x.provider.id));
-  const uniqueProvidersIdsArray = Array.from(uniqueProvidersIdsSet);
 
   const regions = await getDirectusRegions(
     Array.from(uniqueRegionsIds).filter((x) => x !== null),
@@ -245,14 +262,6 @@ export const getStaticProps: GetStaticProps = async ({ locale }) => {
       } as DirectusServiceCategory;
     });
 
-  const providersArray = await getDirectusProviders(
-    directus,
-    DIRECTUS_COUNTRY_ID
-  );
-
-  const providers = providersArray
-    .filter((x) => uniqueProvidersIdsArray.includes(x.id))
-    .sort((a, b) => a.name?.normalize().localeCompare(b.name?.normalize()));
   const populations = await getDirectusPopulationsServed(
     uniquePopulationsIdsArray,
     directus
@@ -270,7 +279,7 @@ export const getStaticProps: GetStaticProps = async ({ locale }) => {
       headerBannerStrings: populateHeaderBannerStrings(dynamicContent),
       socialMediaLinks: populateSocialMediaLinks(dynamicContent),
       serviceMapProps: {
-        services,
+        services: enhancedServices,
         shareButton: getShareButtonStrings(dynamicContent),
         serviceTypes,
         providers,
